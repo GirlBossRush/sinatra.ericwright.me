0d92e662b02b2f9674849c09682a1905135bdff4
Eric Wright <dev@ericwright.me>
2013-04-22 23:38:47 -0400

    Restyle entire site. Wipe layout stylesheets. Add background picture.

--- config/navigation.rb
+++ config/navigation.rb
@@ -1,9 +1,7 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
-  primary.item :whois, "whois", '/'
+  primary.item :whois, "Eric Wright", '/'
   primary.item :work, "work", '/work'
   primary.item :play, "play", "/play"
-  primary.item :contact, "contact", '/contact'
-  primary.item :github, "github", "https://github.com/ericwright90/"
   end
 end
